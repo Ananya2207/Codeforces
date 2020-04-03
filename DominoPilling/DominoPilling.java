fd433200bd08506fbf2340a2328da57153ffc8f1
Ananya <ananya@rknec.edu>
2020-04-04 00:08:08 +0530

    Create DominoPilling.java

---
+++ DominoPilling/DominoPilling.java
@@ -0,0 +1,10 @@
+//package codeforcesa;
+import java.util.*;
+public class DominoPilling {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int M=in.nextInt();
+        int N=in.nextInt();
+        System.out.println((M*N)/2);
+    }
+}
