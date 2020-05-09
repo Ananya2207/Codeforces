4e4aa1a92d65cb2c1e661c8d0b0eae80af9f91d8
Ananya <ananya@rknec.edu>
2020-05-10 01:37:16 +0530

    Create KyoyaAndPhotobooks.java

---
+++ 554A KyoyaAndPhotoBooks/KyoyaAndPhotobooks.java
@@ -0,0 +1,9 @@
+//package codeforcespage2;
+import java.util.*;
+public class KyoyaAndPhotobooks {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        String s=in.nextLine();
+        System.out.println((s.length()+1)*26 - s.length());
+    }
+}
