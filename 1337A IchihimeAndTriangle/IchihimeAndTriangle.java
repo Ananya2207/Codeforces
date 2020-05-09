f24d9bdb3b8e7e4835d64f564a24deb87b646b5f
Ananya <ananya@rknec.edu>
2020-05-10 00:41:55 +0530

    Create IchihimeAndTriangle.java

---
+++ 1337A IchihimeAndTriangle/IchihimeAndTriangle.java
@@ -0,0 +1,16 @@
+//package codeforcespage2;
+import java.util.*;
+public class IchihimeAndTriangle {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++)
+        {
+            int a=in.nextInt();
+            int b=in.nextInt();
+            int c=in.nextInt();
+            int d=in.nextInt();
+            System.out.println(b+" "+c+" "+c);
+        }
+    }
+}
