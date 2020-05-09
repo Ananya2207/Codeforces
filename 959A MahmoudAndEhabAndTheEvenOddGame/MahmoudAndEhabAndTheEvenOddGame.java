b1ba6b2956b4b6a09b7da4d3845ae73d37a11675
Ananya <ananya@rknec.edu>
2020-05-09 13:53:54 +0530

    Create MahmoudAndEhabAndTheEvenOddGame.java

---
+++ 959A MahmoudAndEhabAndTheEvenOddGame/MahmoudAndEhabAndTheEvenOddGame.java
@@ -0,0 +1,16 @@
+//package codeforcespage2;
+import java.util.*;
+public class MahmoudAndEhabAndTheEvenOddGame {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        if(n%2==0)
+        {
+            System.out.println("Mahmoud");
+        }
+        else
+        {
+            System.out.println("Ehab");
+        }
+    }
+}
