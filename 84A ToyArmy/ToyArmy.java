82e916d4d89767ae64ba61e5ba1d3987c65299e7
Ananya <ananya@rknec.edu>
2020-05-12 22:20:43 +0530

    Create ToyArmy.java

---
+++ 84A ToyArmy/ToyArmy.java
@@ -0,0 +1,9 @@
+//package codeforcespage2;
+import java.util.*;
+public class ToyArmy {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        System.out.println(n+n/2);
+    }
+}
