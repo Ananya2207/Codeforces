a5fe59d0fc84d34c4f487a03e18407217374a77b
Ananya <ananya@rknec.edu>
2020-05-21 07:10:08 +0530

    Create AgainTwentyFive.java

---
+++ 630A AgainTwentyFive/AgainTwentyFive.java
@@ -0,0 +1,10 @@
+//package codeforcesa2;
+import java.util.*;
+public class AgainTwentyFive {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        long n=in.nextLong();
+        System.out.println("25");   
+    }
+    
+}
